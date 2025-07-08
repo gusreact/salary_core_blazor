e99d93c3490392ed571483fb7d60c817dede02e5
Villagran Gustavo <gusvillagran@gmail.com>
2025-07-08 08:17:59 -0300

    Fue creado el CRUD para empleados que se utiliza en Datos Generales.

---
+++ SalaryBlazorApp/SalaryBlazorApp.Repository/Migrations/20250707162458_EmployeeTableCreated.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalaryBlazorApp.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmployeeTableCreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empleado",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Legajo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApellidoCasada = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cuil = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cat = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Nacionalidad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Sexo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EstadoCivil = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TipoDocumento = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NroDocumento = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProvinciaEmision = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DomicilioCalle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DomicilioNro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DomicilioPiso = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DomicilioDepto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DomicilioPostal = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Localidad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Provincia = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Situacion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CodigoBaja = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CaracterServicio = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Categoria = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Discapacidad = table.Column<bool>(type: "bit", nullable: false),
+                    DomicilioExplotacion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Observaciones = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleado", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Url",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Url",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Url",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Url",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Url",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Url",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Url",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 32,
+                column: "ParentId",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 33,
+                column: "ParentId",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 34,
+                column: "ParentId",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 35,
+                column: "ParentId",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 36,
+                column: "ParentId",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 37,
+                column: "ParentId",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 38,
+                column: "ParentId",
+                value: 20);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Empleado");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Url",
+                value: "/liquidaciones");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Url",
+                value: "/informes");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Url",
+                value: "/archivos");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Url",
+                value: "/varios");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Url",
+                value: "/consultas");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Url",
+                value: "/ejecutar");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Url",
+                value: "/importacion");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 32,
+                column: "ParentId",
+                value: 21);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 33,
+                column: "ParentId",
+                value: 22);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 34,
+                column: "ParentId",
+                value: 23);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 35,
+                column: "ParentId",
+                value: 24);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 36,
+                column: "ParentId",
+                value: 25);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 37,
+                column: "ParentId",
+                value: 26);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 38,
+                column: "ParentId",
+                value: 27);
+        }
+    }
+}
